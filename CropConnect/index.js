8717f772a74a2f6ba1e7e3af70c90e303d01011a
AdityaParmale <adityaparmale20@gmail.com>
2025-11-27 22:39:57 +0530

    Update index.js

--- CropConnect/index.js
+++ CropConnect/index.js
@@ -1,5 +1,11 @@
 // index.js
+/*
 const { createServer } = require('@vercel/node');
 const app = require('./src/app');
 
 module.exports = createServer(app);
+*/
+// index.js
+module.exports = (req, res) => {
+  res.status(200).send("Vercel deployment is working!");
+};
