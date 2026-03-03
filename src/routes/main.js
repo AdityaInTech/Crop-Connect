eb5c8ba479a300b909a348d91df6847b82516d0d
AdityaParmale <adityaparmale20@gmail.com>
2026-03-03 22:04:39 +0530

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -270,6 +270,52 @@ routes.get("/contact-us", async (req, res) => {
 
 
 
+//routes.post("/process-sell-form", async (req, res) => {
+//    console.log("Form Submitted")
+//    try {
+//        if (!req.session.userId) {
+//          return res.redirect("/login");
+//        }
+        
+        // Execute dynamic upload call assigned by App
+//        req.upload.single('image')(req, res, async function(err) {
+//           if(err){
+//                 console.log("Multer error:", err)
+//                 return res.redirect("/sell")
+//            }
+
+//            const formData = req.body || {};
+//            formData.negotiable = req.body.negotiable ? true : false;
+//            formData.userId = req.session.userId;
+
+//           if (req.file) {
+//                 formData.image = '/uploads/' + req.file.filename; 
+//            }
+
+//            const data = await Sell_form.create(formData);
+//            console.log(data)
+
+//            if (req.file) {
+//              await Slider.create({
+//                title: formData.crop_name || "Farmer Product",
+//                subTitle: `${formData.owner_name || 'User'} posted a new crop!`,
+//                imageUrl: formData.image, 
+//                uploadedBy: req.session.userId
+//              });
+//              console.log("Image added to slider");
+//            }
+//            res.redirect("/sell?status=success");
+//        })
+//    }
+//    catch (error) {
+//        console.log(error)
+//        res.redirect("/sell?status=error");
+//    }
+//})
+
+
+
+
 routes.post("/process-sell-form", async (req, res) => {
     console.log("Form Submitted")
     try {
@@ -277,7 +323,6 @@ routes.post("/process-sell-form", async (req, res) => {
           return res.redirect("/login");
         }
         
-        // Execute dynamic upload call assigned by App
         req.upload.single('image')(req, res, async function(err) {
             if(err){
                  console.log("Multer error:", err)
@@ -312,6 +357,8 @@ routes.post("/process-sell-form", async (req, res) => {
         res.redirect("/sell?status=error");
     }
 })
+
+
 
 
 
