0ae815566945359189c84f68a29952e27ec1434c
AdityaParmale <adityaparmale20@gmail.com>
2026-03-03 22:01:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -66,6 +66,35 @@ app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 //     next();
 // });
 
+
+
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+
+
+
+app.use((req, res, next) => {
+     req.upload = upload;
+     next();
+});
+//app.use('', routes);
+
+
+
+
+
 app.use('', routes)
 
 hbs.registerHelper('inc', v => Number(v) + 1)
